f0e3ba839b1b5440633d8647a4a17aa3f66e71a3
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2022-06-27 13:19:41 -0300

    rotas para clinica criadas - create-read-update

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,24 +6,26 @@ const app = express()
 app.use(express.json())
 
 const pacientes = []
+const clinicas = []
 
 //MiddleWare
 function verifyExistAccountCPF(request, response, next) {
-    const {cpf} = request.params
+    const {cnes} = request.params
 
-    const paciente = pacientes.find((paciente) => paciente.cpf === cpf)
+    const clinica = clinicas.find((clinica) => clinica.cnes == cnes)
 
-    if(!paciente) {
-        return response.status(400).json({error:"paciente not found"})
+    if(!clinica) {
+        return response.status(400).json({error:"clinica not found"})
     }
-    request.paciente = paciente
+    request.clinica = clinica
 
     return next()
 }
 
+
 //Rotas do app
 app.post("/createPaciente", (request, response) => {
-    const {cpf, nome} = request.body
+    const {cpf, nome, favorite} = request.body
     const id = uuidv4()
 
     const pacienteAlreadyExist = pacientes.some(
@@ -36,9 +38,36 @@ app.post("/createPaciente", (request, response) => {
 
     pacientes.push({
         nome,
-        cpf, 
+        cpf,
+        favorite, 
         id,
         statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.post("/createClinica", (request, response) => {
+    const {cnes, nome, cep, cidade, bairro, rua, numero} = request.body
+    const id = uuidv4()
+
+    const pacienteAlreadyExist = clinicas.some(
+        (clinica) => clinica.cnes === cnes
+    )
+
+    if(pacienteAlreadyExist) {
+        return response.status(400).json({error: "clinica already exist!"})
+    }
+
+    clinicas.push({
+        cnes, 
+        id,
+        nome,
+        cep, 
+        cidade, 
+        bairro, 
+        rua, 
+        numero
     })
 
     return response.status(201).send()
@@ -61,12 +90,44 @@ app.post("/addTarefa/:cpf", (request, response) => {
     const statementOperation = {
         nomeTarefa,
         descricao,
-        favorite,
         created_at: new Date(),
         
     }
     paciente.statement.push(statementOperation)
     return response.status(201).send()
+})
+
+app.put("/updatePaciente/:cpf", (request, response)=>{
+    const {name} = request.body
+    const {paciente} = request
+
+    paciente.name = name
+
+    //return response.status(201).send()
+    return response.json(paciente)
+})
+
+
+app.put("/updateClinica/:cnes", (request, response)=>{
+    const {nome, cep, cidade, bairro, rua, numero} = request.body
+    const clinica = request.clinica
+
+    clinica.nome = nome
+    clinica.cep = cep
+    clinica.cidade = cidade,
+    clinica.bairro = bairro,
+    clinica.rua = rua
+    clinica.numero = numero
+
+
+    //return response.status(201).send()
+    return response.json(clinica)
+})
+
+app.get("/consultClinica/:cnes",  (request, response) => {
+    const clinica = request.clinica
+    
+    return response.json(clinica)
 })
 
 app.listen(3333)
